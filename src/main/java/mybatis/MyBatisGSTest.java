7a3df6d2dfc80985bdfa4ab98bb7e745b16209d4
kenu <kenu.heo@gmail.com>
2014-04-11 02:03:33 +0900

    no xml

--- src/main/java/mybatis/MyBatisGSTest.java
+++ src/main/java/mybatis/MyBatisGSTest.java
@@ -1,18 +1,27 @@
 package mybatis;
 
 import java.io.IOException;
-import java.io.InputStream;
 
-import org.apache.ibatis.io.Resources;
+import javax.sql.DataSource;
+
+import org.apache.ibatis.mapping.Environment;
+import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.ibatis.transaction.TransactionFactory;
+import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
 
 public class MyBatisGSTest {
 
 	public static void main(String[] args) throws IOException {
-		String resource = "org/mybatis/example/mybatis-config.xml";
-		InputStream inputStream = Resources.getResourceAsStream(resource);
-		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		DataSource dataSource = BlogDataSourceFactory.getBlogDataSource();
+		TransactionFactory transactionFactory = new JdbcTransactionFactory();
+		Environment environment = new Environment("development",
+				transactionFactory, dataSource);
+		Configuration configuration = new Configuration(environment);
+		configuration.addMapper(BlogMapper.class);
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder()
+				.build(configuration);
 	}
 
 }
