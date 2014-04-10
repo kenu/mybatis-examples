19884bdc55f4ab4d458cdde5920b84b4b3dbf3d5
kenu <kenu.heo@gmail.com>
2014-04-11 02:14:48 +0900

    blogmapper

---
+++ src/main/java/mybatis/BlogMapper.java
@@ -0,0 +1,10 @@
+package mybatis;
+
+public class BlogMapper {
+
+	public Blog selectBlog(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/main/java/mybatis/MyBatisGSTest.java
+++ src/main/java/mybatis/MyBatisGSTest.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
@@ -13,6 +14,14 @@ public class MyBatisGSTest {
 		String resource = "org/mybatis/example/mybatis-config.xml";
 		InputStream inputStream = Resources.getResourceAsStream(resource);
 		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		
+		SqlSession session = sqlSessionFactory.openSession();
+		try {
+		  BlogMapper mapper = session.getMapper(BlogMapper.class);
+		  Blog blog = mapper.selectBlog(101);
+		} finally {
+		  session.close();
+		}
 	}
 
 }
